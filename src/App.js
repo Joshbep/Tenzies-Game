96b5d7fe60b34b90e91328de92f2e20ee283c15a
jbep <joshuabemartin@gmail.com>
2023-06-21 15:56:03 -0400

    feat: created function to generate 10 random numbers form 1-6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import './App.css';
 import Die from './components/Die.js'
 
 function App() {
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+}
+console.log(allNewDice())
   return (
     <main>
       <div className="dice-container">
