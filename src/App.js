7aafc2636d6375d30cc96f61a0d1c6351b54b7b6
jbep <joshuabemartin@gmail.com>
2023-07-06 11:51:24 -0400

    feat: updated the ability to hold dice and roll new ones

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import React, {useState} from 'react';
 function App() {
   const [dice, setDice] = React.useState(allNewDice())
 
+  function generateNewDie() {
+    return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+}
 
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-        newDice.push({
-          value: Math.ceil(Math.random() * 6), 
-          isHeld: false,
-          id: nanoid()
-        })
+        newDice.push(generateNewDie())
     }
     return newDice
   }
@@ -23,7 +26,7 @@ function App() {
     setDice(oldDice => oldDice.map(die => {
         return die.isHeld ? 
             die :
-            {}
+            generateNewDie()
     }))
 }
 
@@ -42,12 +45,14 @@ function App() {
 
   return (
     <main>
-      <div className="dice-container">
-        {diceElements}
-      </div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="dice-container">
+            {diceElements}
+        </div>
+        <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
-  );
+)
 }
 
 export default App;
